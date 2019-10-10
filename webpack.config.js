6f0aa133d88c1be1b112a379f802d3d98eec254c
Samuel Ronce <sam@webcreative5.net>
2019-10-10 12:46:55 +0200

    2.3.0

--- webpack.config.js
+++ webpack.config.js
@@ -19,14 +19,14 @@ module.exports = {
          }]
      },
       plugins: [
-         /* new webpack.optimize.UglifyJsPlugin({
+          new webpack.optimize.UglifyJsPlugin({
               compress: {
                   warnings: false,
               },
               output: {
                   comments: false,
               },
-          }),
-          */
+          })
+          
       ]
  }
